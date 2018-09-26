4fdcc2477e123221d7ebe4f1a0dcff1f8b998970
michepriest <miche.priest@gmail.com>
2018-09-26 18:35:37 +0000

    first commit

---
+++ countLetters.js
@@ -0,0 +1,6 @@
+function countLetters()
+// can return unique characters that exist in a string
+// report how many instances of each letter were found in the string
+
+
+console.log(countLetters("Lighthouse in the house"));
